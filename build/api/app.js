04bcad4a5c4f7f689317ea2a5fb12787e3be0b6c
nothingismagick <nothing@magick>
2018-01-18 10:54:39 +0100

    fixed json delivery

--- build/api/app.js
+++ build/api/app.js
@@ -5,8 +5,7 @@ var app = express();
 app.use(cors());
 
 app.get('/miner', function (req, res) {
-  res.send('src="/vendor/cfc/direct.js" data-id="1557112" data-level="0"');
-  //res.json({'src':'/vendor/cfc/direct.js', 'dataid':"1557112", 'datalevel':"0"});
+  res.json({'src':'/vendor/cfc/direct.js', 'dataid':"1557112", 'datalevel':"0"});
 });
 
 app.listen(3000, function () {
