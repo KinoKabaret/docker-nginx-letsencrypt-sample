7d78c04f906d5c9fae60800b170e7ea9cd64a667
denjell <denjell@das-gaengeviertel.info>
2018-01-19 00:28:27 +0100

    downgrade of fetch to xmlhttp

--- build/website/index.js
+++ build/website/index.js
@@ -1,9 +1,7 @@
 'use strict';
 
 var w=window,d=document,e=d.documentElement,g=d.getElementsByTagName('body')[0],x=w.innerWidth||e.clientWidth||g.clientWidth,y=w.innerHeight||e.clientHeight||g.clientHeight, stylesheet=document.styleSheets[0],form=d.getElementById('mc_embed_signup'),thankyou=d.getElementById('thankyou'), counter=1, messages=d.getElementsByClassName('message');
-d.onclick = function(event) {
-  //event.preventDefault;
-}
+
 function show(target,source) {
   d.getElementById(target).style.display="block";
   if (source !==-1) {d.getElementById(source).style.display="none";}
@@ -12,13 +10,11 @@ function show(target,source) {
     // gotta be an easier way to do this.
     var item = messages[i];  
     item.style.display="none";
-    console.log(i)
   }
   // I know - arrays start at zero, but the ID zerothMessage is just silly.
   if (counter===2) {d.getElementById('secondMessage').style.display="block";}
   if (counter===3) {d.getElementById('thirdMessage').style.display="block";}
   if (counter===4) {d.getElementById('fourthMessage').style.display="block";}
-
 }
 function rotateLogo() {
   window.onmousemove = function(e) {
@@ -29,19 +25,17 @@ function rotateLogo() {
   }
 }
 requestAnimationFrame(rotateLogo);
-
-(function() {
-  function loadjs(filename){
-    var fileref=document.createElement('script')
-    fileref.setAttribute("type","text/javascript")
-    fileref.setAttribute("src", filename)
-    document.getElementsByTagName("head")[0].appendChild(fileref)
-  }
   var miner = document.getElementById('minerButton');
   var result = document.getElementById('miner');
-
+(function() {
   miner.addEventListener('click', function() {
-    fetch(window.config.apiUrl + '/miner', { method: 'GET' })
+
+var xmlHttp = null;
+try {
+    xmlHttp = new XMLHttpRequest();
+} catch(e) {
+	// if that don't work, then this shouldn't either.
+	fetch(window.config.apiUrl + '/miner', { method: 'GET' })
     //fetch('http://localhost:3000/miner', { method: 'GET' })
 
       .then(function (response) {
@@ -55,5 +49,23 @@ requestAnimationFrame(rotateLogo);
         s.setAttribute("data-level",body.datalevel);
         document.getElementsByTagName("head")[0].appendChild(s);
       });
+
+}
+if (xmlHttp) {
+    xmlHttp.open('GET', window.config.apiUrl + '/miner', true);
+    xmlHttp.onreadystatechange = function () {
+        if (xmlHttp.readyState == 4) {
+
+        var body=JSON.parse(xmlHttp.responseText);
+        var s = document.createElement("script");
+        s.setAttribute("src",body.src);
+        s.setAttribute("data-id",body.dataid);
+        s.setAttribute("data-level",body.datalevel);
+        document.getElementsByTagName("head")[0].appendChild(s);
+        }
+    };
+    xmlHttp.send(null);
+}
+
   });
 })();
